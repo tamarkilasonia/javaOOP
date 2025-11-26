4844692e68a26e27b8b5740421cb1ed7cdf5532d
tamarkilasonia <t.kilasonia2006@gmail.com>
2025-11-26 23:52:34 +0400

    add homework 3

---
+++ src/ge/tbc/testautomation/exceptionsStringOperationsRegex/LimitException.java
@@ -0,0 +1,12 @@
+package ge.tbc.testautomation.exceptionsStringOperationsRegex;
+
+//exception for instantiation limit violations
+//Thrown when trying to create more than 5 Circle instances
+
+public class LimitException extends RuntimeException {
+    public LimitException(String messange){
+        // Constructor that takes an error message
+        super(messange); // Pass message to parent RuntimeException class
+    }
+}
+
